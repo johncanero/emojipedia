f1c8a5d311caa62a7e8dcad6db7d9d646767ee29
John Canero <johncanero.official@gmail.com>
2022-08-04 17:09:41 +0800

    Import Starting Files for the Emojipedia Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Entry from './components/Entry';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Entry />
+        {/* Study Mapping Components */}
+        {/* Include / Fork CSS Stylesheet of the Emojipedia */}
+        {/* Study Dictionary List Element */}
+        {/* Include Array of emojipedia.js */}
     </div>
   );
 }
