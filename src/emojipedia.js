8109956b7ddcd80ddd34c38ee56d6d30f62dd367
John Canero <johncanero.official@gmail.com>
2022-08-04 19:01:21 +0800

    Fill the List of Arrays of Emojipedia

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,24 +1,45 @@
 const emojipedia = [
     {
       id: 1,
-      emoji: "💪",
-      name: "Tense Biceps",
+      emoji: "🚀",
+      name: "Rocket",
       meaning:
-        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+        "A rocket is shot into space. “Come on, this is easy. It's not rocket science!“ Or like Buzz Lightyear of Toy Story said: To infinity and much farther."
     },
     {
       id: 2,
-      emoji: "🙏",
-      name: "Person With Folded Hands",
+      emoji: "🏂",
+      name: "Snowboarder",
       meaning:
-        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+        "The person is sliding over the snow standing on a snowboard. In 1900, the predecessor of today's snowboard was invented by an Austrian. Symbol for winter: whether vacation, sports, activities or mountains."
     },
     {
       id: 3,
-      emoji: "🤣",
-      name: "Rolling On The Floor, Laughing",
+      emoji: "⚽",
+      name: "Soccer Ball",
       meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        "I want to play soccer or “The game is starting!“ The black-and-white ball is mostly used in conjunction with European soccer."
+    },
+    {
+      id: 4,
+      emoji: "🌱",
+      name: "Seedling",
+      meaning:
+        "I love gardening! Most plants are dibbled in spring. The delicate plant needs to be nourished and cared for. Can symbolize growth or a fresh start."
+    },
+    {
+      id: 5,
+      emoji: "🏖",
+      name: "Beach",
+      meaning:
+        "The parasol on a beach section by the sea stands for beach holidays and hot weather. Let's go to the lake and enjoy the sun!."
+    },
+    {
+      id: 6,
+      emoji: "🐶",
+      name: "Dog Face ",
+      meaning:
+        "A sweet dog face with a panting tongue and floppy ears. Stands for unconditional loyalty and attention. Can refer to your own pet (“I love my dog.“) or to people (“You sly dog!“)."
     }
   ];
 
